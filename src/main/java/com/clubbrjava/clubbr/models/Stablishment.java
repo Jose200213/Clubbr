60d542d031d1b6bf1154bb14dd7f88b15a8f581f
jaimexry <jaime.bellidomedina@alum.uca.es>
2023-11-09 19:23:55 +0100

    Creación de la clase JwtUtil
    Esta clase permite crear tokens a partir de un id de usuario y su nivel en el sistema.

--- src/main/java/com/clubbrjava/clubbr/models/Stablishment.java
+++ src/main/java/com/clubbrjava/clubbr/models/Stablishment.java
@@ -7,7 +7,7 @@ import java.util.List;
 import lombok.Getter;
 
 @Entity
-public class Local {
+public class Stablishment {
     @Id @Getter
     private Long localID, capacity;
 
---
+++ src/main/java/com/clubbrjava/clubbr/utils/JwtUtil.java
@@ -0,0 +1,58 @@
+package com.clubbrjava.clubbr.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expirationInMs}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(String id, String userLevel) {
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder().setId(id)
+                .setSubject(userLevel)
+                .setIssuedAt(now)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getSubjectFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public String getIdFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getId();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
