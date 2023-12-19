42c47ca1d93c582bc63db110fed52c8f8a25fe86
Parejo <100793729+Parejo00@users.noreply.github.com>
2023-12-19 15:25:14 +0100

    Update payment.java

--- src/main/java/com/Clubbr/Clubbr/Entity/payment.java
+++ src/main/java/com/Clubbr/Clubbr/Entity/payment.java
@@ -1,28 +1,13 @@
 package com.Clubbr.Clubbr.Entity;
-<<<<<<< Updated upstream
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.*;
 import lombok.*;
-=======
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.Date;
-
->>>>>>> Stashed changes
 
 @Entity
 @Getter
 @Setter
-<<<<<<< Updated upstream
-@Table(name = "payment")
-=======
 @Table(name = "paymentRepository")
->>>>>>> Stashed changes
 @NoArgsConstructor
 @AllArgsConstructor
 public class payment {
@@ -32,17 +17,8 @@ public class payment {
     @Column(name = "paymentID")
     private Long paymentID;
 
-<<<<<<< Updated upstream
-    @OneToOne
-    @JoinColumn(name = "workerID", referencedColumnName = "workerID")
-    @JsonBackReference
-    private worker worker;
-
-    @Column(name = "finalPayment")
-    private float finalPayment;
-=======
     @ManyToOne
-    @JoinColumn(name = "userID", referencedColumnName = "userID")
+    @JoinColumn(name = "userID")
     private user userID;
 
     @Column(name = "amount")
@@ -50,5 +26,9 @@ public class payment {
 
     @Column(name = "paymentDate")
     private Date paymentDate;
->>>>>>> Stashed changes
+
+    // Puedes agregar más campos según tus necesidades
+
+    // Constructores, getters y setters
+
 }
