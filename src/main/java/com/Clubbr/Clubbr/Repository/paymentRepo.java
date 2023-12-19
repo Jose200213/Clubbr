7fe2caf9482ba35470730caa06857ac0e76b6b33
Parejo <100793729+Parejo00@users.noreply.github.com>
2023-12-19 15:27:53 +0100

    Update paymentRepo.java

--- src/main/java/com/Clubbr/Clubbr/Repository/paymentRepo.java
+++ src/main/java/com/Clubbr/Clubbr/Repository/paymentRepo.java
@@ -1,7 +1,6 @@
 package com.Clubbr.Clubbr.Repository;
 
 import com.Clubbr.Clubbr.Entity.payment;
-<<<<<<< Updated upstream
 import com.Clubbr.Clubbr.Entity.worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,12 +8,5 @@ import java.util.Optional;
 
 public interface paymentRepo extends JpaRepository<payment, Long>{
     Optional<payment> findByWorkerID(worker worker);
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface paymentRepo extends JpaRepository<payment, Long>{
-
->>>>>>> Stashed changes
 }
