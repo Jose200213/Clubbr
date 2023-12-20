2e79aa18df14290c4f9fe5f45fa4cabe2ed0db6d
DiZaster2002 <carlosdurgon2002@gmail.com>
2023-12-20 16:37:14 +0100

    Actualización Carpeta Front-End

---
+++ src/Front-End/src/tickets.js
@@ -0,0 +1,22 @@
+export const tickets = [
+    {   
+        id : '001',
+        name : 'Placeholder event name',
+        schedule : 'dd/mm -> h/min a h/min'
+    },
+    {   
+        id : '002',
+        name : 'Placeholder event name',
+        schedule : 'dd/mm -> h/min a h/min'
+    },
+    {   
+        id : '003',
+        name : 'Placeholder event name',
+        schedule : 'dd/mm -> h/min a h/min'
+    },
+    {   
+        id : '004',
+        name : 'Placeholder event name',
+        schedule : 'dd/mm -> h/min a h/min'
+    },
+]
